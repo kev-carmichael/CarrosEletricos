2dfff27535906aa5115a7928232b54c0cfe1ce19
Kev Carmichael <kev.carmichael22@gmail.com>
2022-06-16 15:01:57 +0100

    added code to MainActivity.kt

--- app/src/main/java/com/example/carroseletricos/MainActivity.kt
+++ app/src/main/java/com/example/carroseletricos/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.example.carroseletricos
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnMarca: Button = findViewById(R.id.btnMarca)
+        btnMarca.setOnClickListener {
+            val makeIntent = Intent(this, MakeActivity::class.java)
+            startActivity(makeIntent)
+        }
+
+        val btnPesquisa: Button = findViewById(R.id.btnPesquisa)
+        btnMarca.setOnClickListener {
+            val filterIntent = Intent(this, FilterActivity::class.java)
+            startActivity(filterIntent)
+        }
+
+
+
     }
 }
