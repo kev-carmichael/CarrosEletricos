f589bf9fc06c8df7018d2ff063d8b08d822fe91d
Kev Carmichael <kev.carmichael22@gmail.com>
2022-06-21 16:31:45 +0100

    added 2 audi car photos

--- app/src/main/java/com/example/carroseletricos/data/CarImages.kt
+++ app/src/main/java/com/example/carroseletricos/data/CarImages.kt
@@ -4,7 +4,7 @@ import com.example.carroseletricos.R
 
 class CarImages {
     val carMap = mapOf(
-        1 to R.drawable.outline, 2 to R.drawable.outline,
+        1 to R.drawable.audietron, 2 to R.drawable.audietrongt,
         3 to R.drawable.outline, 4 to R.drawable.outline,
         5 to R.drawable.bmwi4, 6 to R.drawable.outline,
         7 to R.drawable.outline, 8 to R.drawable.outline,
