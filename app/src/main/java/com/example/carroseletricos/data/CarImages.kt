31931bb41c36aef010089c52e6e52a1da1101040
Kev Carmichael <kev.carmichael22@gmail.com>
2022-06-21 17:30:37 +0100

    added 15 photos

--- app/src/main/java/com/example/carroseletricos/data/CarImages.kt
+++ app/src/main/java/com/example/carroseletricos/data/CarImages.kt
@@ -5,14 +5,14 @@ import com.example.carroseletricos.R
 class CarImages {
     val carMap = mapOf(
         1 to R.drawable.audietron, 2 to R.drawable.audietrongt,
-        3 to R.drawable.outline, 4 to R.drawable.outline,
-        5 to R.drawable.bmwi4, 6 to R.drawable.outline,
-        7 to R.drawable.outline, 8 to R.drawable.outline,
-        9 to R.drawable.outline, 10 to R.drawable.outline,
-        11 to R.drawable.outline, 12 to R.drawable.outline,
-        13 to R.drawable.outline, 14 to R.drawable.outline,
-        15 to R.drawable.outline, 16 to R.drawable.outline,
-        17 to R.drawable.outline, 18 to R.drawable.outline,
+        3 to R.drawable.audiq4etron, 4 to R.drawable.bmwi3,
+        5 to R.drawable.bmwi4, 6 to R.drawable.bmwix3,
+        7 to R.drawable.citroenec4, 8 to R.drawable.citroeneberlingo,
+        9 to R.drawable.citroenespacetourer, 10 to R.drawable.cupraborn,
+        11 to R.drawable.daciaspring, 12 to R.drawable.ds3,
+        13 to R.drawable.fiat500, 14 to R.drawable.fordmustangmache,
+        15 to R.drawable.hondae, 16 to R.drawable.hyundaiioniq,
+        17 to R.drawable.hyundaiioniq5, 18 to R.drawable.hyundaikauai,
         200 to R.drawable.vwid3
     )
 }
